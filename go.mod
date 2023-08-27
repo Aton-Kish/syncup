1db9da6cacf4fdc7f2deed0fdd7bfda1631fa11d
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2023-08-27 13:46:39 +0000

    feat(lib): added interface for survey

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Aton-Kish/syncup
 go 1.21.0
 
 require (
+	github.com/AlecAivazis/survey/v2 v2.2.13 // NOTE: pinned to v2.2.13 due to https://github.com/go-survey/survey/issues/368
 	github.com/Aton-Kish/aws-credscache-go v0.2.0
 	github.com/aws/aws-sdk-go-v2 v1.21.0
 	github.com/aws/aws-sdk-go-v2/config v1.18.37
@@ -24,7 +25,14 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.21.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
+	golang.org/x/text v0.3.3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/syncup/interface/console/survey.go
@@ -0,0 +1,85 @@
+// Copyright (c) 2023 Aton-Kish
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package console
+
+import (
+	"context"
+
+	"github.com/AlecAivazis/survey/v2"
+)
+
+type isurvey interface {
+	InputString(ctx context.Context, prompt *survey.Input, opts ...survey.AskOpt) (string, error)
+	InputInt(ctx context.Context, prompt *survey.Input, opts ...survey.AskOpt) (int, error)
+	Password(ctx context.Context, prompt *survey.Password, opts ...survey.AskOpt) (string, error)
+	Confirm(ctx context.Context, prompt *survey.Confirm, opts ...survey.AskOpt) (bool, error)
+	Select(ctx context.Context, prompt *survey.Select, opts ...survey.AskOpt) (string, error)
+	MultiSelect(ctx context.Context, prompt *survey.MultiSelect, opts ...survey.AskOpt) ([]string, error)
+}
+
+type xsurvey struct {
+	options []survey.AskOpt
+}
+
+func newSurvey(opts ...survey.AskOpt) isurvey {
+	return &xsurvey{
+		options: opts,
+	}
+}
+
+func (s *xsurvey) InputString(ctx context.Context, prompt *survey.Input, opts ...survey.AskOpt) (string, error) {
+	return askOne[string](s, ctx, prompt, opts...)
+}
+
+func (s *xsurvey) InputInt(ctx context.Context, prompt *survey.Input, opts ...survey.AskOpt) (int, error) {
+	return askOne[int](s, ctx, prompt, opts...)
+}
+
+func (s *xsurvey) Password(ctx context.Context, prompt *survey.Password, opts ...survey.AskOpt) (string, error) {
+	return askOne[string](s, ctx, prompt, opts...)
+}
+
+func (s *xsurvey) Confirm(ctx context.Context, prompt *survey.Confirm, opts ...survey.AskOpt) (bool, error) {
+	return askOne[bool](s, ctx, prompt, opts...)
+}
+
+func (s *xsurvey) Select(ctx context.Context, prompt *survey.Select, opts ...survey.AskOpt) (string, error) {
+	return askOne[string](s, ctx, prompt, opts...)
+}
+
+func (s *xsurvey) MultiSelect(ctx context.Context, prompt *survey.MultiSelect, opts ...survey.AskOpt) ([]string, error) {
+	return askOne[[]string](s, ctx, prompt, opts...)
+}
+
+func askOne[T any](s *xsurvey, ctx context.Context, prompt survey.Prompt, opts ...survey.AskOpt) (T, error) {
+	optFns := make([]survey.AskOpt, 0, len(s.options)+len(opts))
+	optFns = append(optFns, s.options...)
+	optFns = append(optFns, opts...)
+
+	var res T
+	if err := survey.AskOne(prompt, &res, optFns...); err != nil {
+		var zero T
+
+		return zero, err
+	}
+
+	return res, nil
+}
