3cb437a2313e5aa0f93b6fcd13c908094cd88c26
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2023-08-27 11:15:05 +0000

    test(lib): added unit tests for SchemaAppSyncRepository.Save()

--- internal/syncup/interface/infrastructure/schema_appsync_test.go
+++ internal/syncup/interface/infrastructure/schema_appsync_test.go
@@ -23,13 +23,24 @@ package infrastructure
 import (
 	"context"
 	"errors"
+	"net/http"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/Aton-Kish/syncup/internal/syncup/domain/model"
 	mock_infrastructure "github.com/Aton-Kish/syncup/internal/syncup/interface/infrastructure/mock"
 	"github.com/Aton-Kish/syncup/internal/testhelpers"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
+	"github.com/aws/aws-sdk-go-v2/aws/retry"
+	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/appsync"
+	"github.com/aws/aws-sdk-go-v2/service/appsync/types"
+	smithymiddleware "github.com/aws/smithy-go/middleware"
+	smithyhttp "github.com/aws/smithy-go/transport/http"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
@@ -133,6 +144,584 @@ func Test_schemaAppSyncRepository_Get(t *testing.T) {
 
 			// Act
 			actual, err := r.Get(ctx, tt.args.apiID)
+
+			// Assert
+			if tt.expected.errAs == nil && tt.expected.errIs == nil {
+				assert.Equal(t, tt.expected.out, actual)
+
+				assert.NoError(t, err)
+			} else {
+				assert.Nil(t, actual)
+
+				if tt.expected.errAs != nil {
+					assert.ErrorAs(t, err, &tt.expected.errAs)
+				}
+
+				if tt.expected.errIs != nil {
+					assert.ErrorIs(t, err, tt.expected.errIs)
+				}
+			}
+		})
+	}
+}
+
+func Test_schemaAppSyncRepository_Save(t *testing.T) {
+	testdataBaseDir := "../../../../testdata"
+	schema := model.Schema(testhelpers.MustReadFile(t, filepath.Join(testdataBaseDir, "schema/schema.graphqls")))
+	duration := time.Duration(1) * time.Millisecond
+
+	type fields struct {
+		duration time.Duration
+	}
+
+	type args struct {
+		apiID  string
+		schema *model.Schema
+	}
+
+	type mockAppSyncClientStartSchemaCreationReturn struct {
+		out *appsync.StartSchemaCreationOutput
+		err error
+	}
+	type mockAppSyncClientStartSchemaCreation struct {
+		calls   int
+		returns []mockAppSyncClientStartSchemaCreationReturn
+	}
+
+	type mockAppSyncClientGetSchemaCreationStatusReturn struct {
+		out *appsync.GetSchemaCreationStatusOutput
+		err error
+	}
+	type mockAppSyncClientGetSchemaCreationStatus struct {
+		calls   int
+		returns []mockAppSyncClientGetSchemaCreationStatusReturn
+	}
+
+	type mockAppSyncClientGetIntrospectionSchemaReturn struct {
+		out *appsync.GetIntrospectionSchemaOutput
+		err error
+	}
+	type mockAppSyncClientGetIntrospectionSchema struct {
+		calls   int
+		returns []mockAppSyncClientGetIntrospectionSchemaReturn
+	}
+
+	type expected struct {
+		out   *model.Schema
+		errAs error
+		errIs error
+	}
+
+	tests := []struct {
+		name    string
+		timeout time.Duration
+		fields  fields
+		args    args
+		mockAppSyncClientStartSchemaCreation
+		mockAppSyncClientGetSchemaCreationStatus
+		mockAppSyncClientGetIntrospectionSchema
+		expected expected
+	}{
+		{
+			name:    "happy path: default",
+			timeout: 100 * duration, // not timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: &schema,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{
+					{
+						out: &appsync.StartSchemaCreationOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusSuccess,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{
+					{
+						out: &appsync.GetIntrospectionSchemaOutput{
+							Schema: []byte(schema),
+						},
+						err: nil,
+					},
+				},
+			},
+			expected: expected{
+				out:   &schema,
+				errAs: nil,
+				errIs: nil,
+			},
+		},
+		{
+			name:    "happy path: retries on ConcurrentModificationException",
+			timeout: 100 * duration, // not timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: &schema,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{
+					{
+						out: nil,
+						err: &awshttp.ResponseError{
+							ResponseError: &smithyhttp.ResponseError{
+								Response: &smithyhttp.Response{Response: &http.Response{StatusCode: 409}},
+								Err:      &types.ConcurrentModificationException{},
+							},
+						},
+					},
+					{
+						out: &appsync.StartSchemaCreationOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusSuccess,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{
+					{
+						out: &appsync.GetIntrospectionSchemaOutput{
+							Schema: []byte(schema),
+						},
+						err: nil,
+					},
+				},
+			},
+			expected: expected{
+				out:   &schema,
+				errAs: nil,
+				errIs: nil,
+			},
+		},
+		{
+			name:    "edge path: timeout",
+			timeout: duration, // timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: &schema,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{
+					{
+						out: &appsync.StartSchemaCreationOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{},
+			},
+			expected: expected{
+				out:   nil,
+				errAs: &model.LibError{},
+				errIs: context.DeadlineExceeded,
+			},
+		},
+		{
+			name:    "edge path: exceeds max retry count",
+			timeout: 100 * duration, // not timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: &schema,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{
+					{
+						out: nil,
+						err: &awshttp.ResponseError{
+							ResponseError: &smithyhttp.ResponseError{
+								Response: &smithyhttp.Response{Response: &http.Response{StatusCode: 409}},
+								Err:      &types.ConcurrentModificationException{},
+							},
+						},
+					},
+					{
+						out: nil,
+						err: &awshttp.ResponseError{
+							ResponseError: &smithyhttp.ResponseError{
+								Response: &smithyhttp.Response{Response: &http.Response{StatusCode: 409}},
+								Err:      &types.ConcurrentModificationException{},
+							},
+						},
+					},
+					{
+						out: nil,
+						err: &awshttp.ResponseError{
+							ResponseError: &smithyhttp.ResponseError{
+								Response: &smithyhttp.Response{Response: &http.Response{StatusCode: 409}},
+								Err:      &types.ConcurrentModificationException{},
+							},
+						},
+					},
+				},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{},
+			},
+			expected: expected{
+				out:   nil,
+				errAs: &model.LibError{},
+				errIs: nil,
+			},
+		},
+		{
+			name:    "edge path: nil schema",
+			timeout: 100 * duration, // not timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: nil,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{},
+			},
+			expected: expected{
+				out:   nil,
+				errAs: &model.LibError{},
+				errIs: nil,
+			},
+		},
+		{
+			name:    "edge path: appsync.StartSchemaCreation() error",
+			timeout: 100 * duration, // not timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: &schema,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{
+					{
+						out: nil,
+						err: errors.New("error"),
+					},
+				},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{},
+			},
+			expected: expected{
+				out:   nil,
+				errAs: &model.LibError{},
+				errIs: nil,
+			},
+		},
+		{
+			name:    "edge path: failed to create",
+			timeout: 100 * duration, // not timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: &schema,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{
+					{
+						out: &appsync.StartSchemaCreationOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusFailed,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{},
+			},
+			expected: expected{
+				out:   nil,
+				errAs: &model.LibError{},
+				errIs: model.ErrCreateFailed,
+			},
+		},
+		{
+			name:    "edge path: invalid schema status",
+			timeout: 100 * duration, // not timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: &schema,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{
+					{
+						out: &appsync.StartSchemaCreationOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatus("invalid schema status"),
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{},
+			},
+			expected: expected{
+				out:   nil,
+				errAs: &model.LibError{},
+				errIs: model.ErrInvalidValue,
+			},
+		},
+		{
+			name:    "edge path: appsync.GetSchemaCreationStatus() error",
+			timeout: 100 * duration, // not timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: &schema,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{
+					{
+						out: &appsync.StartSchemaCreationOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+					{
+						out: nil,
+						err: errors.New("error"),
+					},
+				},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{},
+			},
+			expected: expected{
+				out:   nil,
+				errAs: &model.LibError{},
+				errIs: nil,
+			},
+		},
+		{
+			name:    "edge path: appsync.GetIntrospectionSchema() error",
+			timeout: 100 * duration, // not timeout
+			fields: fields{
+				duration: duration,
+			},
+			args: args{
+				apiID:  "apiID",
+				schema: &schema,
+			},
+			mockAppSyncClientStartSchemaCreation: mockAppSyncClientStartSchemaCreation{
+				returns: []mockAppSyncClientStartSchemaCreationReturn{
+					{
+						out: &appsync.StartSchemaCreationOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetSchemaCreationStatus: mockAppSyncClientGetSchemaCreationStatus{
+				returns: []mockAppSyncClientGetSchemaCreationStatusReturn{
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusProcessing,
+						},
+						err: nil,
+					},
+					{
+						out: &appsync.GetSchemaCreationStatusOutput{
+							Status: types.SchemaStatusSuccess,
+						},
+						err: nil,
+					},
+				},
+			},
+			mockAppSyncClientGetIntrospectionSchema: mockAppSyncClientGetIntrospectionSchema{
+				returns: []mockAppSyncClientGetIntrospectionSchemaReturn{
+					{
+						out: nil,
+						err: errors.New("error"),
+					},
+				},
+			},
+			expected: expected{
+				out:   nil,
+				errAs: &model.LibError{},
+				errIs: nil,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arrange
+			ctx, cancel := context.WithTimeout(context.Background(), tt.timeout)
+			defer cancel()
+
+			cfg, err := config.LoadDefaultConfig(
+				ctx,
+				config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("key", "secret", "session")),
+				config.WithAPIOptions([]func(stack *smithymiddleware.Stack) error{
+					func(stack *smithymiddleware.Stack) error {
+						return stack.Finalize.Add(
+							smithymiddleware.FinalizeMiddlewareFunc("Mock", func(ctx context.Context, input smithymiddleware.FinalizeInput, next smithymiddleware.FinalizeHandler) (smithymiddleware.FinalizeOutput, smithymiddleware.Metadata, error) {
+								switch awsmiddleware.GetOperationName(ctx) {
+								case "StartSchemaCreation":
+									defer func() { tt.mockAppSyncClientStartSchemaCreation.calls++ }()
+									r := tt.mockAppSyncClientStartSchemaCreation.returns[tt.mockAppSyncClientStartSchemaCreation.calls]
+									return smithymiddleware.FinalizeOutput{Result: r.out}, smithymiddleware.Metadata{}, r.err
+								case "GetSchemaCreationStatus":
+									defer func() { tt.mockAppSyncClientGetSchemaCreationStatus.calls++ }()
+									r := tt.mockAppSyncClientGetSchemaCreationStatus.returns[tt.mockAppSyncClientGetSchemaCreationStatus.calls]
+									return smithymiddleware.FinalizeOutput{Result: r.out}, smithymiddleware.Metadata{}, r.err
+								case "GetIntrospectionSchema":
+									defer func() { tt.mockAppSyncClientGetIntrospectionSchema.calls++ }()
+									r := tt.mockAppSyncClientGetIntrospectionSchema.returns[tt.mockAppSyncClientGetIntrospectionSchema.calls]
+									return smithymiddleware.FinalizeOutput{Result: r.out}, smithymiddleware.Metadata{}, r.err
+								default:
+									t.Fatal("unexpected operation")
+									return smithymiddleware.FinalizeOutput{}, smithymiddleware.Metadata{}, nil
+								}
+							}), smithymiddleware.After,
+						)
+					},
+				}),
+				config.WithRetryer(func() aws.Retryer {
+					return retry.AddWithMaxBackoffDelay(retry.NewStandard(), duration)
+				}),
+			)
+			assert.NoError(t, err)
+
+			mockAppSyncClient := appsync.NewFromConfig(cfg)
+
+			r := &schemaAppSyncRepository{
+				appsyncClient: mockAppSyncClient,
+				duration:      tt.fields.duration,
+			}
+
+			// Act
+			actual, err := r.Save(ctx, tt.args.apiID, tt.args.schema)
 
 			// Assert
 			if tt.expected.errAs == nil && tt.expected.errIs == nil {
