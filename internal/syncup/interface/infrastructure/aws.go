06dbc54878a55fe38c02bc72cefc6e71a115421f
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2023-08-27 11:19:42 +0000

    test(lib): removed aws sdk mocks

--- internal/syncup/interface/infrastructure/aws.go
+++ internal/syncup/interface/infrastructure/aws.go
@@ -18,8 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-//go:generate mockgen -source=$GOFILE -destination=./mock/mock_$GOFILE
-
 package infrastructure
 
 import (
